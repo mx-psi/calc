16bdb03693efb30d31741999f5787c0b72aed3a6
pbaeyens <pbaeyens31@gmail.com>
2015-04-02 21:39:18 +0200

    Reorders and generalizes functions.

--- parser.hs
+++ parser.hs
@@ -10,38 +10,21 @@ instance Show Expression where
   show (Val x)      = show x
   show (Op op list) = "(" ++ intercalate (" "++op++" ") (map show list) ++ ")"
 
----- Operations and related functions ----
-
-binOps = [("+",(+)), ("*",(*)), ("^",(**))] -- List of binary operations
-binSym = map fst binOps
-unOps  = [("-",(0-)), ("/",(1/))] -- List of unary operations
--- / is the inverse function
-unSym  = map fst unOps
-
--- Given symbol, return associated operation
-unOp :: String -> (Double -> Double)
-unOp = unOp' unOps
-  where
-    unOp' [] _ = error "unOp: not recognized"
-    unOp' (op:ops) symbol
-      | (fst op) == symbol = snd op
-      |  otherwise         = unOp' ops symbol
-
-binOp :: String -> (Double -> Double -> Double)
-binOp = binOp' binOps
-  where
-    binOp' [] _ = error "binOp: not recognized"
-    binOp' (op:ops) symbol
-      | (fst op) == symbol = snd op
-      |  otherwise         = binOp' ops symbol
-
----- Basic list functions ----
+---- Basic functions ----
 
 slice :: (Int, Int) -> [a] -> [a]
 -- Returns slice of lists between indices.
 -- Uncurry version to simplify calls
-slice (from,to) _ | to < from = []
-slice (from,to) xs = take (to - from + 1 :: Int) (drop from xs)
+slice (from,to) | to < from = \_ -> []
+slice (from,to) = take (to - from + 1 :: Int) . drop from
+
+-- Removes brackets
+removeBrackets ys = slice (1,(length ys)-2) ys
+
+-- Adds n to pair
+add n (f,s) = (f + n, s + n)
+
+---- Functions for replacing ----
 
 replace :: Eq a => [a] -> [a] -> [a] -> [a]
 -- Replaces sublist in list
@@ -52,16 +35,23 @@ replace sub rep ys@(x:xs)
   |sub `isPrefixOf` ys      = rep ++ replace sub rep (drop (length sub) ys)
   |otherwise                = x:(replace sub rep xs)
 
+substitute :: Expression -> Expression -> Expression -> Expression
+-- Replaces Expressions.
+substitute sub rep (Op op ys)
+  | sub /= (Op op ys) = Op op (map (substitute sub rep) ys)
+substitute sub rep val  = if (sub == val) then rep else val
+
+applyAssoc  :: (a -> a -> b -> b) -> [(a, a)] -> b -> b
+applyAssoc f = foldl (.) id . map (\(s,r) -> f s r)
+
 replaceList :: (Eq a) => [([a],[a])] -> [a] -> [a]
 -- Replaces in given order each pair of sublists in list
-replaceList       []       ys = ys
-replaceList ((sub,rep):xs) ys = replaceList xs (replace sub rep ys)
+replaceList = applyAssoc replace
 
--- Removes brackets
-removeBrackets ys = slice (1,(length ys)-2) ys
+substituteList :: [(Expression, Expression)] -> Expression -> Expression
+-- Replaces list of expressions in order.
+substituteList = applyAssoc substitute
 
--- Adds n to pair
-add n (f,s) = (f+n,s+n)
 
 ---- Brackets handling ----
 
@@ -99,17 +89,6 @@ aux = map (\c->[c]) ['A'..] -- Auxiliary variables to replace brackets
 
 ---- Expression data type related functions ----
 
-substitute :: Expression -> Expression -> Expression -> Expression
--- Replaces Expressions.
-substitute sub rep (Op op ys)
-  | sub /= (Op op ys) = Op op (map (substitute sub rep) ys)
-substitute sub rep val  = if (sub == val) then rep else val
-
-substituteList :: [(Expression, Expression)] -> Expression -> Expression
--- Replaces list of expressions in order.
-substituteList  [] = id
-substituteList ((sub,rep):xs) = substituteList xs . substitute sub rep
-
 fromString :: [String] -> String -> Expression
 -- Converts string to expression given list of binary operations
 fromString _ "" = Val 0
@@ -125,6 +104,21 @@ fromString opl@(op:ops) ys
 fromString _ ys
   | all (\x -> (isDigit x) || (x `elem` ".-")) ys = Val (read ys :: Double)
   | otherwise                                    = Var ys
+
+---- Operations and related functions ----
+
+-- nOps: assoc list of symbols and related functions, ordered by precedence
+-- nSym: List of function symbols
+-- nOp: Given symbol, return associated function
+
+binOps = [("+",(+)), ("*",(*)), ("^",(**))]
+binSym = map fst binOps
+binOp op = fromJust (lookup op binOps)
+
+unOps  = [("-",(0-)), ("/",(1/))]
+unSym  = map fst unOps
+unOp op = fromJust (lookup op unOps)
+
 
 readExp = fromString binSym . replaceList [(" ",""),("-","+-"),("/","*1/")]
 
